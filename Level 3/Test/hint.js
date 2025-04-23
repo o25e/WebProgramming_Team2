0cb4bdee80bdc69b5c90d1cd08bb510fe6c40501
Sangho Lee <leesangho1016@gmail.com>
2025-04-23 21:29:29 +0900

    hint 수정

--- Level 3/Test/hint.js
+++ Level 3/Test/hint.js
@@ -5,6 +5,8 @@ const HIDDEN_CLASSNAME = "hidden";
 // 힌트 보여주는 함수
  function showHint(){
     window.open('web_example1.html');
+    localStorage.setItem(`level3Completed`, 'true');
+
     // if(hintText.classList.contains(HIDDEN_CLASSNAME))
     //     hintText.classList.remove(HIDDEN_CLASSNAME);
     // else
