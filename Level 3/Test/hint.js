b69b94deb4c833e7154cc9ba20b135561eb3704b
Sangho Lee <leesangho1016@gmail.com>
2025-04-23 20:47:45 +0900

    힌트 수정

---
+++ Level 3/Test/hint.js
@@ -0,0 +1,14 @@
+const hintBtn = document.querySelector("#hintBtn");
+// const hintText = document.querySelector("#hintText");
+const HIDDEN_CLASSNAME = "hidden";
+
+// 힌트 보여주는 함수
+ function showHint(){
+    window.open('web_example1.html');
+    // if(hintText.classList.contains(HIDDEN_CLASSNAME))
+    //     hintText.classList.remove(HIDDEN_CLASSNAME);
+    // else
+    //     hintText.classList.add(HIDDEN_CLASSNAME);
+}
+// 힌트 버튼 클릭하면 힌트 보여줌
+hintBtn.addEventListener("click", showHint);
